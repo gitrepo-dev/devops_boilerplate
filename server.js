4a09d639b1fca5934f91b8d17799376d762b2bef
jay sean <jaysean.developer@gmail.com>
2023-07-28 22:16:52 +0530

    d

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const http = require("http")
 http.createServer((req, res)=>{
   res.write("hello world")
   res.end()
-  console.log("server is runing 50")
+  console.log("server is runing 500")
 }).listen(5000)
